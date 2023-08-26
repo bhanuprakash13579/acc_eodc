866f7316bf2308d520a9b0cad0651de5f4fbc580
bhanuprakash13579 <bhanuprakashnaidu13579@gmail.com>
2023-08-26 22:43:34 +0530

    for checking

--- acc_eodc/settings.py
+++ acc_eodc/settings.py
@@ -160,15 +160,15 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # SMTP settings
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT =  587                                                                   #############################
+EMAIL_PORT=587                                                                   #############################
 #EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-#EMAIL_HOST_USER = os.environ.get("sender_email")     #############################
-EMAIL_HOST_USER = 'bhanuprakashnaidu13579@gmail.com'
-#EMAIL_HOST_PASSWORD = os.environ.get("sender_email_pwd")             #############################
-EMAIL_HOST_PASSWORD = 'grwfoxfjhzgcscgk'
+EMAIL_HOST_USER = os.environ.get("sender_email")     #############################
+# EMAIL_HOST_USER = 'bhanuprakashnaidu13579@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get("sender_email_pwd")             #############################
+# EMAIL_HOST_PASSWORD = 'grwfoxfjhzgcscgk' #dummy password
 
 # If DEBUG is True, Django will serve static files during development
 if DEBUG:
